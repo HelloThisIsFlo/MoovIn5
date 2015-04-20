6b37a01f1ce847ff84dfbe078721d54295a8fd74
Shock_N745 <Shockn745@gmail.com>
2015-04-21 00:43:21 +0200

    Add cardview with reveal effet. Lots of bug with the reveal effect

--- app/src/main/java/com/shockn745/workoutmotivationaltool/motivation/MotivationActivity.java
+++ app/src/main/java/com/shockn745/workoutmotivationaltool/motivation/MotivationActivity.java
@@ -1,7 +1,11 @@
 package com.shockn745.workoutmotivationaltool.motivation;
 
+import android.animation.Animator;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.widget.ImageButton;
 import android.widget.Toolbar;
 
 import com.shockn745.workoutmotivationaltool.R;
@@ -13,6 +17,8 @@ import com.shockn745.workoutmotivationaltool.R;
  * @author Florian Kempenich
  */
 public class MotivationActivity extends Activity {
+
+    private ImageButton mAddCardButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +36,38 @@ public class MotivationActivity extends Activity {
         setActionBar(mToolbar);
 
         // Add the navigation arrow
-
         // Inspection removed, because it won't throw NullPointerException since the actionBar is
         // initialized just above.
-
         //noinspection ConstantConditions
         getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Find view by id
+        mAddCardButton = (ImageButton) findViewById(R.id.add_card_button);
+
+        mAddCardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View addCardView = findViewById(R.id.card_view); //TODO remove
+
+                // get the center for the clipping circle
+//                int cx = addCardView.getWidth();
+//                int cy = addCardView.getHeight();
+
+                int cx = addCardView.getLeft();
+                int cy = addCardView.getTop();
+
+                // get the final radius for the clipping circle
+                int finalRadius = 1300; // TODO change hardcoded test value
+
+                // create the animator for this view (the start radius is zero)
+                Animator anim =
+                        ViewAnimationUtils.createCircularReveal(addCardView, cx, cy, 0, finalRadius);
+
+                // make the view visible and start the animation
+                addCardView.setVisibility(View.VISIBLE);
+                anim.setDuration(1000);
+                anim.start();
+            }
+        });
     }
 }
