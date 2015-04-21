520f322691678046a37f0460295fa79ff02499cf
Shock_N745 <Shockn745@gmail.com>
2015-04-21 15:11:19 +0200

    Fix bug and Add reverse animation

--- app/src/main/java/com/shockn745/workoutmotivationaltool/motivation/MotivationActivity.java
+++ app/src/main/java/com/shockn745/workoutmotivationaltool/motivation/MotivationActivity.java
@@ -3,6 +3,7 @@ package com.shockn745.workoutmotivationaltool.motivation;
 import android.animation.Animator;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.widget.ImageButton;
@@ -18,7 +19,10 @@ import com.shockn745.workoutmotivationaltool.R;
  */
 public class MotivationActivity extends Activity {
 
+    private static final String LOG_TAG = MotivationActivity.class.getSimpleName();
+
     private ImageButton mAddCardButton;
+    private boolean mAddCardMenuDisplayed = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,29 +48,80 @@ public class MotivationActivity extends Activity {
         // Find view by id
         mAddCardButton = (ImageButton) findViewById(R.id.add_card_button);
 
+
         mAddCardButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                View addCardView = findViewById(R.id.card_view); //TODO remove
+                if (!mAddCardMenuDisplayed) {
+                    // Show the addCardMenu
+                    mAddCardMenuDisplayed = true;
+                    View addCardMenu = findViewById(R.id.card_view);
 
-                // get the center for the clipping circle
-//                int cx = addCardView.getWidth();
-//                int cy = addCardView.getHeight();
+                    // Get the center for the clipping circle
+                    /// Get dimensions
+                    int width = (int) getResources().getDimension(R.dimen.add_card_menu_width);
+                    int height = (int) getResources().getDimension(R.dimen.add_card_menu_height);
+                    /// Start circle at top right corner
+                    int cx = width;
+                    int cy = 0;
+                    Log.d(LOG_TAG, "cx = " + cx);
+                    Log.d(LOG_TAG, "cy = " + cy);
 
-                int cx = addCardView.getLeft();
-                int cy = addCardView.getTop();
+                    // Get the final radius for the clipping circle
+                    int finalRadius = (int) Math.sqrt(Math.pow(height, 2) + Math.pow(width, 2));
 
-                // get the final radius for the clipping circle
-                int finalRadius = 1300; // TODO change hardcoded test value
+                    // Create the animator for this view (the start radius is zero)
+                    Animator anim = ViewAnimationUtils
+                            .createCircularReveal(addCardMenu, cx, cy, 0, finalRadius);
 
-                // create the animator for this view (the start radius is zero)
-                Animator anim =
-                        ViewAnimationUtils.createCircularReveal(addCardView, cx, cy, 0, finalRadius);
+                    // Make the view visible and start the animation
+                    addCardMenu.setVisibility(View.VISIBLE);
+                    anim.setDuration(500);
+                    anim.start();
+                } else {
+                    // Hide the addCardMenu
 
-                // make the view visible and start the animation
-                addCardView.setVisibility(View.VISIBLE);
-                anim.setDuration(1000);
-                anim.start();
+                    mAddCardMenuDisplayed = false;
+                    final View addCardMenu = findViewById(R.id.card_view);
+
+                    // Get the center for the clipping circle
+                    /// Get dimensions
+                    int width = (int) getResources().getDimension(R.dimen.add_card_menu_width);
+                    int height = (int) getResources().getDimension(R.dimen.add_card_menu_height);
+                    /// Start circle at top right corner
+                    int cx = width;
+                    int cy = 0;
+                    Log.d(LOG_TAG, "cx = " + cx);
+                    Log.d(LOG_TAG, "cy = " + cy);
+
+                    // Get the final radius for the clipping circle
+                    int startRadius = (int) Math.sqrt(Math.pow(height, 2) + Math.pow(width, 2));
+
+                    // Create the animator for this view (the start radius is zero)
+                    Animator anim =ViewAnimationUtils
+                            .createCircularReveal(addCardMenu, cx, cy, startRadius, 0);
+
+                    // Add listener to hide the view when animation is done
+                    anim.addListener(new Animator.AnimatorListener() {
+
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            // Hide the addCardMenu
+                            addCardMenu.setVisibility(View.GONE);
+                        }
+
+                        @Override
+                        public void onAnimationStart(Animator animation) {}
+                        @Override
+                        public void onAnimationCancel(Animator animation) {}
+                        @Override
+                        public void onAnimationRepeat(Animator animation) {}
+                    });
+
+                    // Start the animation
+                    anim.setDuration(500);
+                    anim.start();
+                }
             }
         });
     }
