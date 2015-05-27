eae23f75563074ac8e6511e903968f2d75e018e2
Shock_N745 <Shockn745@gmail.com>
2015-05-27 17:26:43 +0200

    Add compat to API 16 + fix layout problem on step 1&2 of tutorial

--- app/src/main/java/com/shockn745/moovin5/motivation/MotivationActivity.java
+++ app/src/main/java/com/shockn745/moovin5/motivation/MotivationActivity.java
@@ -95,7 +95,7 @@ public class MotivationActivity extends AppCompatActivity implements FABCallback
 
             // Init the AdView
             mAdView = new AdView(this);
-            mAdView.setAdUnitId(getString(R.string.banner_ad_unit_id));
+            mAdView.setAdUnitId(getString(R.string.banner_ad_unit_id_debug));
             mAdView.setAdSize(AdSize.MEDIUM_RECTANGLE);
             AdRequest adRequest = new AdRequest.Builder().build();
             mAdView.loadAd(adRequest);
